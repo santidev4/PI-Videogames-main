f7d3a008390a27e35af15b5365b3934f83c840f7
santiago <santiago.borquez.1705@gmail.com>
2022-05-09 18:48:58 -0300

    arrancando client

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
+import LandingPage from './components/LandingPage/LandingPage';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Henry Videogames</h1>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+      </div>
+        <Routes>
+            <Route exact path='/' element= {<LandingPage/>} />
+        </Routes>
+    </BrowserRouter>
   );
 }
 
