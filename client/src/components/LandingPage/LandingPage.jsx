88969db00453cf852161e84560f54a19044a4c0b
santiago <santiago.borquez.1705@gmail.com>
2022-05-10 13:01:40 -0300

    haciendo el css de landing

--- client/src/components/LandingPage/LandingPage.jsx
+++ client/src/components/LandingPage/LandingPage.jsx
@@ -1,14 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "./LandingPage.module.css"
+import style from "./LandingPage.module.css"
 
 export default function LandingPage() {
     return(
-        <div>
-            <h1 >Bienvenidos</h1>
-            <Link to='/home'>
-                <button>Ingresar</button>
-            </Link>
-        </div>
+            <div className={style.container}>
+                <img src="https://images.pexels.com/photos/6686141/pexels-photo-6686141.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260" alt="" className={style.bg_img} />
+                <div className={style.container_data}>
+                    <div className={style.data}>
+                        <h1 className={style.titulo}>Bienvenidos</h1>
+                        <p className={style.description}>Acá podrás buscar tus juegos favoritos</p>
+                        <Link to='/home'>
+                            <button className={style.btn}>Ingresar</button>
+                        </Link>
+                    </div>
+                </div>
+            </div>
     )
 }
