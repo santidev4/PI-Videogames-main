d5f593750b3082b7e0714ddbf9a032f8df57b228
santiago <santiago.borquez.1705@gmail.com>
2022-05-11 11:28:19 -0300

    landing de function a class

--- client/src/components/LandingPage/LandingPage.jsx
+++ client/src/components/LandingPage/LandingPage.jsx
@@ -2,9 +2,26 @@ import React from "react";
 import { Link } from "react-router-dom";
 import style from "./LandingPage.module.css"
 
-export default function LandingPage() {
-    return(
-            <div className={style.container}>
+// export default function LandingPage() {
+//     return(
+//             <div className={style.container}>
+//                 <img src="https://images.pexels.com/photos/6686141/pexels-photo-6686141.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260" alt="" className={style.bg_img} />
+//                 <div className={style.container_data}>
+//                     <div className={style.data}>
+//                         <h1 className={style.titulo}>Bienvenidos</h1>
+//                         <p className={style.description}>Acá podrás buscar tus juegos favoritos</p>
+//                         <Link to='/home' className={style.link}>
+//                             <span className={style.btn}>Ingresar</span>
+//                         </Link>
+//                     </div>
+//                 </div>
+//             </div>
+//     )
+// };
+
+class LandingPage extends React.Component {
+    render() {
+        return <div className={style.container}>
                 <img src="https://images.pexels.com/photos/6686141/pexels-photo-6686141.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260" alt="" className={style.bg_img} />
                 <div className={style.container_data}>
                     <div className={style.data}>
@@ -16,5 +33,7 @@ export default function LandingPage() {
                     </div>
                 </div>
             </div>
-    )
+    }
 }
+
+export default LandingPage;
