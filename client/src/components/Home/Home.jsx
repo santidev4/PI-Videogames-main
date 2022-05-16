bb96a3b59e7aa3193db75482fe12d3f5e388b0d6
santiago <santiago.borquez.1705@gmail.com>
2022-05-16 17:37:44 -0300

    search

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -2,13 +2,15 @@ import React from "react";
 // import { useEffect } from "react";
 // import { useDispatch, useSelector } from "react-redux";
 // import { getVideogames } from "../../redux/actions";
-import Cards from "../Cards/Cards"
+import Cards from "../Cards/Cards";
+import NavBar from "../NavBar/NavBar"
 
 
 export default function Home(){
 
     return(
         <>
+            <NavBar />
             <Cards />
         </>
     )
---
+++ client/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import SearchBar from "../SearchBar/SearchBar";
+
+export default function NavBar(){
+    return(
+        <SearchBar />
+    )
+}
