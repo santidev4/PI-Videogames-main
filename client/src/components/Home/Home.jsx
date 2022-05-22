91b8e6e5da98f90919d5a85b1c68f1840289eee6
santiago <santiago.borquez.1705@gmail.com>
2022-05-22 10:27:47 -0300

    making pagination

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,8 +1,9 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getVideogames, getGenres } from "../../redux/actions";
 import Cards from "../Cards/Cards";
-import NavBar from "../NavBar/NavBar"
+import NavBar from "../NavBar/NavBar";
+import Pagination from "../Pagination/Pagination";
 
 
 export default function Home(){
@@ -10,6 +11,9 @@ export default function Home(){
     const dispatch = useDispatch();
     const allVideogames = useSelector(state => state.videogames);
     const genres = useSelector(state => state.genres);
+
+    const [currentPage, setCurrentPage] = useState(1);
+    const [videogamesPerPage, setVidegoamesPerPage] = useState(15);
 
     useEffect(() => {
         dispatch(getVideogames());
@@ -19,7 +23,9 @@ export default function Home(){
         dispatch(getGenres());
     }, [dispatch]);
     
-    
+    const paginationFunction = (pageNumber) => {
+        setCurrentPage(pageNumber)
+    }
     
     const tenVideogames = allVideogames.slice(0, 15);
 
@@ -27,6 +33,10 @@ export default function Home(){
     return(
         <>
             <NavBar genres={genres} />
+            <Pagination 
+            videogamesPerPage={videogamesPerPage}
+            allVideogames={allVideogames.length}
+            paginationFunction={paginationFunction} />
             <Cards data={tenVideogames} />
         </>
     )
--- client/src/components/Pagination/Pagination.jsx
+++ client/src/components/Pagination/Pagination.jsx
@@ -1,10 +1,23 @@
 import React from "react";
 
-export default function Pagination(){
+export default function Pagination({videogamesPerPage, allVideogames, paginationFunction}){
+    const pageNumbers = [];
+    for (let i = 1; i < Math.ceil(allVideogames/videogamesPerPage); i++) {
+        pageNumbers.push(i);
+    }
 
     return (
         <div>
-            
+            {
+                pageNumbers?.map(number => (
+                    <button 
+                    onClick={() => paginationFunction(number)}
+                    key={number}
+                    className="">
+                        {number}
+                    </button>
+                ))
+            }
         </div>
     )
 }
